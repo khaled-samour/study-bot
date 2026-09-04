55f49bc90e20f244c3eb9630083b7489dd2fdb87
khaled-samour <kh322382@gmail.com>
2026-09-04 22:03:18 +0300

    Implement simple HTTP server in main.py
    Added a simple HTTP server that responds with 'Bot is alive!'

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import os
 import time
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
+
+def run_fake_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHandler)
+    server.serve_forever()
+
+server_thread = threading.Thread(target=run_fake_server, daemon=True)
+server_thread.start()
+
 from google import genai
 import telebot
 
